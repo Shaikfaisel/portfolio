c915527bbac5af425785858acc781d32370dfa6b
Shaikfaisel <shaikfaisel129@gmail.com>
2026-07-24 23:56:24 +0530

    Fix Render deployment
    git push origin main#

--- app.py
+++ app.py
@@ -100,4 +100,9 @@ def send_message():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
